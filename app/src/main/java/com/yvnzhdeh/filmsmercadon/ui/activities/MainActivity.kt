db1f0d7e1a02377a97c9a7bedb1c3e4b20b082dc
Nzhdeh <nzhdeh1991@gmail.com>
2022-09-30 19:33:54 +0200

    First commit

--- app/src/main/java/com/yvnzhdeh/filmsmercadon/ui/activities/MainActivity.kt
+++ app/src/main/java/com/yvnzhdeh/filmsmercadon/ui/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.yvnzhdeh.filmsmercadon
+package com.yvnzhdeh.filmsmercadon.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.yvnzhdeh.filmsmercadon.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
